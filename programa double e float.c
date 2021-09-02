b53fd582e361eb32baf1e78d33fa308e38000228
LuzianoSilva <88911452+LuzianoSilva@users.noreply.github.com>
2021-09-02 15:04:25 -0300

    Add files via upload

---
+++ programa double e float.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{//var
+float pi = 3.14;
+double piDouble = 3.1415926535897932384626433832795;
+//saidas
+printf("Valor de pi %.5f\n", pi );
+printf("Valor de pi mais preciso %.7f\n", piDouble );
+return 0;
+}
---
+++ salario.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+int salario = 5;
+printf("Eu pretendo ganhar %d mil reais como programador C", salario);
+return 0;
+}
