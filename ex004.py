f4aea47fdade540606c51c5e5d848ce1042a97f5
LuzianoSilva <88911452+LuzianoSilva@users.noreply.github.com>
2021-09-02 15:00:59 -0300

    Add files via upload

---
+++ ex004.py
@@ -0,0 +1,7 @@
+v1 = str(input("escreva seu nome: "))
+v2 = str(input("escreva seu sobrenome: "))
+v3 = int(input("digite um número inteiro: "))
+v4 = int(input("digite outro número inteiro: "))
+x = v1+v2
+y = v3+v4
+print(x,y )
