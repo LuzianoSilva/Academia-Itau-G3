39803f224a18fca946fb540df48354a4d6fe072c
LuzianoSilva <88911452+LuzianoSilva@users.noreply.github.com>
2021-09-15 23:17:45 -0300

    Add files via upload
    Quer testar se é maior de idade? Ou menor de idade? O programa te diz!

---
+++ Programa que diz se vc é maior ou menor de idade.py
@@ -0,0 +1,5 @@
+n1 = int(input("Coloque uma idade:"))
+if n1 > 18:
+    print(f"Você tem {n1} anos de idade e é maior de idade.")
+else:
+    print(f"Você tem {n1} anos de idade e não é maior de idade.")
